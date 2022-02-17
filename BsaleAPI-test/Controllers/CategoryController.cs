90e03c63d8e902ae059a99946f75600a1e446a4c
Rodrigo <Rodrigo@LAPTOP-CS2C463N>
2022-02-16 23:52:09 -0300

    Creación de controladores

---
+++ BsaleAPI-test/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using BsaleAPI_test.Data;
+using BsaleAPI_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsaleAPI_test.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            return Ok(await _categoryRepository.GetAllCategories());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllCategories(int id)
+        {
+            return Ok(await _categoryRepository.GetCategoryDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var created = await _categoryRepository.InsertCategory(category);
+            return Created("created", created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory([FromBody] Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var created = await _categoryRepository.UpdateCategory(category);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _categoryRepository.DeleteCategory(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BsaleAPI-test/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using BsaleAPI_test.Data;
+using BsaleAPI_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BsaleAPI_test.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return Ok(await _productRepository.GetAllProducts());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllProducts(int id)
+        {
+            return Ok(await _productRepository.GetProductDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var created = await _productRepository.InsertProduct(product);
+            return Created("created", created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var created = await _productRepository.UpdateProduct(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            await _productRepository.DeleteProduct(id);
+            return NoContent();
+        }
+    }
+}
